ee5b281f9fa306170c278c3b70f8b68f6f026fdc
gutsfullhd <antoniocaraballo04@iesamachado.org>
2025-01-21 20:44:25 +0100

    connection.php

---
+++ proyectoiaw/connection.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "sql308.thsite.top";
+$username = "thsi_38097486";
+$password = "!uH!cRwa";
+$dbname = "thsi_38097486_bdiesantoniomachadotonycg";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    Echo "Conexion exitosa"
+
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
